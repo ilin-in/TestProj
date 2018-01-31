93e15288af9ea84794ba72b7f873d36ccc6a8fe7
Ilya Ilin <ilya_ilin@epbyminw0928.minsk.epam.com>
2018-01-31 16:28:44 +0300

    Add a distance when creating a new object

--- TestProj/View Models/LocationsViewModel.swift
+++ TestProj/View Models/LocationsViewModel.swift
@@ -19,6 +19,7 @@ class LocationsViewModel {
     fileprivate let locationManager = CLLocationManager()
     fileprivate var location: Observable<CLLocation>? = nil
     fileprivate let utilityScheduler = ConcurrentDispatchQueueScheduler(qos: .utility)
+    fileprivate var lastUsedCoord: CLLocation? = nil
     
     init() {
         let realm = try! Realm()
@@ -28,6 +29,12 @@ class LocationsViewModel {
         if realmLocations.count == 0 {
             appendInitialData()
         }
+        
+        Observable.combineLatest(locationManager.rx.didUpdateLocations.map { $0[0] }
+            .filter { $0.horizontalAccuracy < kCLLocationAccuracyHundredMeters}, locations) { (coords, locs) -> String in
+                print(1)
+                return ""
+        }.subscribe().disposed(by: disposeBag)
     }
     
     func addNewLocation(name: String, lat: Double, lng: Double) {
@@ -37,6 +44,10 @@ class LocationsViewModel {
         location.lat = lat
         location.lng = lng
         location.name = name
+        if let lastCoords = self.lastUsedCoord {
+            location.distance = LocationsViewModel.getDistance(from: CLLocation(latitude: lat, longitude: lng), to: lastCoords)
+            location.distanceString = LocationsViewModel.distanceFormat(distance: location.distance)
+        }
         try! realm.write {
             realm.add(location)
         }
@@ -53,26 +64,32 @@ class LocationsViewModel {
     }
     
     fileprivate func startUpdatingLocation() {
-        let scheduler = SerialDispatchQueueScheduler(qos: .userInteractive)
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         locationManager.rx.didUpdateLocations.map { $0[0] }
             .filter { $0.horizontalAccuracy < kCLLocationAccuracyHundredMeters}
-            .observeOn(scheduler)
+            .observeOn(SerialDispatchQueueScheduler(qos: .userInteractive))
             .subscribe(onNext: { (location) in
+                self.lastUsedCoord = location
+                self.locationManager.stopUpdatingLocation()
                 let realm = try! Realm()
                 let realmLocations = realm.objects(Location.self)
                 try! realm.write {
                     for realmLocation in realmLocations {
-                        // TODO: Include more accurate measuring
-                        let distance = CLLocation(latitude: realmLocation.lat, longitude: realmLocation.lng).distance(from: location)
-                        realmLocation.distance = distance
-                        realmLocation.distanceString = String.init(format: NSLocalizedString("%.2f km", comment: ""), distance / 1000)
+                        realmLocation.distance = LocationsViewModel.getDistance(from: CLLocation(latitude: realmLocation.lat, longitude: realmLocation.lng), to: location)
+                        realmLocation.distanceString = LocationsViewModel.distanceFormat(distance: realmLocation.distance)
                     }
                 }
-                print(Thread.current.debugDescription)
             })
             .disposed(by: disposeBag)
     }
     
+    fileprivate class func getDistance(from: CLLocation, to: CLLocation) -> CLLocationDistance {
+        // TODO: Apply more accurate measuring
+        return to.distance(from: from)
+    }
+    
+    fileprivate class func distanceFormat(distance: CLLocationDistance) -> String {
+        return String.init(format: NSLocalizedString("%.2f km", comment: ""), distance / 1000)
+    }
 }
